5d0c27201f9b491691ef9a7db3695fbf88f1939a
bytecod31 <emwiti658@gmail.com>
2023-01-10 02:38:43 +0300

    added a test class

--- common.py
+++ common.py
@@ -22,7 +22,7 @@ Color = StructClass()
 
 Color.PANEL = '#545454'
 Color.OFF = '#656565'
-Color.On = '#00FF33'
+Color.ON = '#00FF33'
 Color.WARN = '#FFCC00'
 Color.ALARM = '#FF4422'
 
@@ -95,6 +95,121 @@ class LED:
 
             elif orient == POINT_LEFT:
                 self.light = self.canvas.create_polygon(x-d, y,x+d,y+d,x+d,y-d,x-d,y,outline=outline)
+
+        self.canvas.pack(side=TOP, fill=X, expand=NO)
+        self.update()
+
+    def turnon(self):
+        self.status = STATUS_ON
+        if not self.blink: self.update()
+
+    def turnoff(self):
+        self.status = STATUS_OFF
+        if not self.blink: self.update()
+
+    def alarm(self):
+        self.status = STATUS_ALARM
+        if not self.blink: self.update()
+
+    def warn(self):
+        self.status = STATUS_WARN
+        if not self.blink: self.update()
+
+    def set(self, color):
+        self.status = STATUS_SET
+        self.specialColor = color
+        self.update()
+
+    def blinkon(self):
+        if not self.blink:
+            self.blink = 1
+            self.onState = self.status
+            self.update()
+
+    def blinkoff(self):
+        if self.blink:
+            self.blink = 0
+            self.status = self.onState
+            self.onState = None
+            self.on = 0
+            self.update()
+
+    def blinkstate(self, blinkstate):
+        if blinkstate:
+            self.blinkon()
+        else:
+            self.blinkoff()
+
+    def update(self):
+        # first blink the LED, if blink is set to ON
+        if self.blink:
+            if self.on:
+                if not self.onState:
+                    self.onState = self.status
+
+                self.status = STATUS_OFF
+                self.on = 0
+            else:
+                if self.onState:
+                    self.status = self.onState # current ON color
+
+                self.on = 1
+
+        if self.status == STATUS_ON:
+            self.canvas.itemconfig(self.light, fill=self.onColor)
+        elif self.status == STATUS_OFF:
+            self.canvas.itemconfig(self.light, fill=self.offColor)
+        elif self.status == STATUS_WARN:
+            self.canvas.itemconfig(self.light, fill=self.warningColor)
+        elif self.status == STATUS_SET:
+            self.canvas.itemconfig(self.light, fill=self.specialColor)
+        else:
+            self.canvas.itemconfig(self.light, fill=self.alarmColor)
+
+        self.canvas.update_idletasks()
+
+        if self.blink:
+            self.frame.after(self.blinkrate * 500, self.update)
+
+
+if __name__ == "__main__":
+    class TestLEDS(Frame):
+        def __init__(self, parent=None):
+            # list of colors and blink ON/OFF
+            states = [(STATUS_OFF, 0),
+                      (STATUS_ON, 0),
+                      (STATUS_WARN, 0),
+                      (STATUS_ALARM, 0),
+                      (STATUS_SET, 0),
+
+                      (STATUS_ON, 1),
+                      (STATUS_WARN, 1),
+                      (STATUS_ALARM, 1),
+                      (STATUS_SET, 1)]
+
+            # list of LED types to display, with their attributes
+            leds = [(ROUND, 25, 25, FLAT, 0, None, ""),
+                    (ROUND, 15, 15, RAISED, 1, None, ""),
+                    (SQUARE, 20, 20, SUNKEN, 1, None, "")]
+
+            Frame.__init__(self)  # init superclass
+            self.pack()
+            self.master.title('LEDs')
+
+            # iterate for each type of LED
+            for shape, w, h, app, bd, orient, outline in leds:
+                frame = Frame(self, bg=Color.PANEL)
+                frame.pack(anchor=N, expand=YES, fill=X)
+
+                # iterate for the selected states
+                for state, blink in states:
+                    LED(frame, shape=shape, status=state, width=w, height=h, appearance=app, orient=orient, blink=blink, bd=bd,outline=outline).frame.pack(side=LEFT, expand=YES, padx=1, pady=1)
+
+    TestLEDS().mainloop()
+
+
+
+
 
 
 
